43646e4e88fb77a1ccdd4dcc1f5410e49e01c6eb
eriton <bytyqieriton58@gmail.com>
2026-04-15 23:31:19 +0200

    add startup script and fix envv

--- start.sh
+++ start.sh
@@ -2,8 +2,13 @@
 
 echo "STARTING APP"
 
+php artisan config:clear
+php artisan cache:clear
 php artisan optimize:clear
+
+echo "ENV TEST:"
+printenv | grep DB_
+
 php artisan migrate --force
-php artisan migrate:status
 
 php artisan serve --host=0.0.0.0 --port=${PORT:-8080}

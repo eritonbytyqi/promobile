6d336d24d54f903edd2474e8ac0707087b00ac31
eriton <bytyqieriton58@gmail.com>
2026-04-16 18:57:46 +0200

    update railway start script

--- start.sh
+++ start.sh
@@ -10,7 +10,13 @@ php artisan cache:clear
 echo "ENV DB CHECK"
 printenv | grep DB_
 
-echo "RUN MIGRATIONS"
+echo "MIGRATION STATUS BEFORE"
+php artisan migrate:status || true
+
+echo "RUN FRESH MIGRATIONS"
 php artisan migrate:fresh --force
+
+echo "MIGRATION STATUS AFTER"
+php artisan migrate:status || true
 
 php artisan serve --host=0.0.0.0 --port=${PORT:-8080}
